3498a88942980bc110b5b3ca66ebc0b50574e253
PrabhleenKaur28 <prabhleen544@gmail.com>
2025-05-21 01:36:41 +0530

    feat: add logger middleware for request logging

---
+++ middleware/logger.go
@@ -0,0 +1,15 @@
+package middleware
+
+import (
+	"log"
+	"net/http"
+	"time"
+)
+
+func Logger(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		log.Printf("[%s] %s %s", r.Method, r.URL.Path, time.Since(start))
+		next(w, r)
+	}
+}
