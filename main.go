bbc6d96416907b7a85ed4d2722e3b7071b8ca910
PrabhleenKaur28 <prabhleen544@gmail.com>
2025-05-21 01:41:24 +0530

    fix: corrected module path and import issues

--- main.go
+++ main.go
@@ -1,20 +1,28 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
+	"fmt"
+	"net/http"
+
+	"github.com/PrabhleenKaur28/HTTP_Server/handlers"
+	"github.com/PrabhleenKaur28/HTTP_Server/middleware"
 )
 
-func helloHandler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintln(w, "Hello, world!")
-}
-
 func main() {
-    http.HandleFunc("/", helloHandler)
+	mux := http.NewServeMux()
 
-    fmt.Println("Server is running at http://localhost:8080")
-    err := http.ListenAndServe(":8080", nil)
-    if err != nil {
-        fmt.Println("Error starting server:", err)
-    }
+	// Static files
+	fs := http.FileServer(http.Dir("./static"))
+	mux.Handle("/static/", http.StripPrefix("/static", fs))
+
+	// API routes
+	mux.HandleFunc("/", middleware.Logger(handlers.HomeHandler))
+	mux.HandleFunc("/api/projects", middleware.Logger(handlers.ProjectsHandler))
+	mux.HandleFunc("/contact", middleware.Logger(handlers.ContactHandler))
+
+	fmt.Println("Server is running at http://localhost:8080")
+	err := http.ListenAndServe(":8080", mux)
+	if err != nil {
+		fmt.Println("Server failed:", err)
+	}
 }
