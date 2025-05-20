b19bd717fb8aaf83f9f4dd81c1cf3c92808b34d2
PrabhleenKaur28 <prabhleen544@gmail.com>
2025-05-21 03:53:49 +0530

    feat: connected PostgreSQL and store contact form data

--- main.go
+++ main.go
@@ -32,10 +32,8 @@ func main() {
 
 	// API routes
 	mux.HandleFunc("/", middleware.Logger(handlers.HomeHandler))
-	mux.HandleFunc("/api/projects", middleware.Logger(handlers.ProjectsHandler))
 	mux.HandleFunc("/contact", middleware.Logger(handlers.ContactHandler))
 	mux.HandleFunc("/admin/messages", middleware.Logger(handlers.MessagesHandler))
-
 
 	fmt.Println("Server is running at http://localhost:8080")
 	err = http.ListenAndServe(":8080", mux)
