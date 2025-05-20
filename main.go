0898efc3438538a7475b16c02658f5b4df318582
PrabhleenKaur28 <prabhleen544@gmail.com>
2025-05-21 01:19:27 +0530

    feat: basic HTTP server that prints Hello

--- main.go
+++ main.go
@@ -6,17 +6,15 @@ import (
 )
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hello, World!")
-}
-
-func aboutHandler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "This is the About page.")
+    fmt.Fprintln(w, "Hello, world!")
 }
 
 func main() {
     http.HandleFunc("/", helloHandler)
-    http.HandleFunc("/about", aboutHandler)
 
-    fmt.Println("Server is starting at http://localhost:8080")
-    http.ListenAndServe(":8080", nil)
+    fmt.Println("Server is running at http://localhost:8080")
+    err := http.ListenAndServe(":8080", nil)
+    if err != nil {
+        fmt.Println("Error starting server:", err)
+    }
 }
